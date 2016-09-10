6d8cf31cc74f58db1b7de62b97d2f38c8bf9b1a5
Scott Thompson <sjthom05@gmail.com>
2016-09-10 17:52:04 -0400

    Plan for what we are going to do

---
+++ SpoilerRevealer/js/app.js
@@ -0,0 +1,10 @@
+//Prevent spoilerphobes from seeing spoilers
+//Solution: Hide spoilers and reveal them through user interaction
+
+//1 Hide spoiler
+
+//2 Add a button
+
+//3 When the button is pressed
+    //3.1 Show spoiler
+    //3.2 Get rid of the button
