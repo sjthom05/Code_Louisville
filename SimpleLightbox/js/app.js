747db282c640225b6443f0746d90f2f0e44153a7
Scott Thompson <sjthom05@gmail.com>
2016-09-10 19:32:07 -0400

    When you click on an image add it to the overlay

--- SimpleLightbox/js/app.js
+++ SimpleLightbox/js/app.js
@@ -2,21 +2,32 @@
 //Solution: Create an overlay with the large image - Lightbox
 
 var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+
+//add image to overlay
+$overlay.append($image);
 
 //Add ovrlay
 $("body").append($overlay);
-    //an image
+    
     //a caption
 
 //Capture the click event on a link to an image
 $("#imageGallery a").click(function(event){
     event.preventDefault();
-    var href = $(this).attr("href");
-    $overlay.show();
-    //Show the overlay
+    var immageLocation = $(this).attr("href");
     //Update overlay with the image linked in the link
+    $image.attr('src', immageLocation);
+
+    //Show the overlay
+    $overlay.show();
+    
+    
     //Get child's alt attribute and set caption'
 });
 
 //When overlay is clicked
+$overlay.click(function(){
+    $overlay.hide();
+});
     //Hide the overlay
