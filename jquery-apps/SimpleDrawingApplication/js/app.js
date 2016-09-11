803e7dbf10df8f2c15beec54fa624a51fa29838f
Scott Thompson <sjthom05@gmail.com>
2016-09-10 23:16:29 -0400

    Can now select a color

--- jquery-apps/SimpleDrawingApplication/js/app.js
+++ jquery-apps/SimpleDrawingApplication/js/app.js
@@ -1,9 +1,18 @@
 //Problem: No user interaction causes no change to applicaiton
 //Solution: When user interacts cause changes appropriately
 
+var color = $(".selected").css('background-color');
+
 //When clicking on control list items
+$(".controls li").click(function(){
     //Deselect sibling elements
+    $(this).siblings().removeClass('selected');
     //Select clicked element
+    $(this).addClass('selected')
+    //cache current color
+    color = $(this).css('background-color');
+});
+    
 
 //When new color is pressed
     //Show #colorselect or hide #colorselect
