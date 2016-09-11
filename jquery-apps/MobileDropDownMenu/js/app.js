63e5043116b9791c6c9c936b9cc704c4287a7e9b
Scott Thompson <sjthom05@gmail.com>
2016-09-10 20:04:11 -0400

    Plan out what the app will be doing

---
+++ jquery-apps/MobileDropDownMenu/js/app.js
@@ -0,0 +1,20 @@
+//Problem: It look gross in smaller browser widths and small devices
+//Solution: To Hide the text links and swap them out with a more appropriate navigation
+
+//Create a select and append to menu
+
+//Cycle over menu links
+    //Create an option
+    //Option's value is the href'
+    //Option's text is the text of link'
+    //Append option to select
+
+//Create button 
+
+//Bind click to button 
+    //go to select's location
+
+//Modify CSS to hide links on small width and show buton and select
+    //Also hides select and button on larger width and show's links
+
+//Deal with selected options depending on current page
