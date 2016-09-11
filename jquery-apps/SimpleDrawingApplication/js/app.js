14d94da408b54744d4da49f9d0f88dc2187b6b5f
Scott Thompson <sjthom05@gmail.com>
2016-09-10 23:51:31 -0400

    Update new color select

--- jquery-apps/SimpleDrawingApplication/js/app.js
+++ jquery-apps/SimpleDrawingApplication/js/app.js
@@ -12,12 +12,24 @@ $(".controls li").click(function(){
     //cache current color
     color = $(this).css('background-color');
 });
-    
 
 //When new color is pressed
+$('#revealColorSelect').click(function(){
     //Show #colorselect or hide #colorselect
+    changeColor();
+    $('#colorSelect').toggle();
+});
+
+//update the new color span
+function changeColor () {
+    var r = $('#red').val();
+    var g = $('#green').val();
+    var b = $('#blue').val();
+    $('#newColor').css("background-color", "rgb("+r+","+g+","+b+")");
+}
 
 //When color sliders change
+$('input[type=range]').change(changeColor)
     //update the new color span
 
 //When add color is pressed 
