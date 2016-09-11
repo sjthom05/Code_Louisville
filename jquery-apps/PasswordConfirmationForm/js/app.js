2b81630c8664e0f4a8dee005c49b0ffd7c668d2e
Scott Thompson <sjthom05@gmail.com>
2016-09-10 21:14:45 -0400

    added confirm password event

--- jquery-apps/PasswordConfirmationForm/js/app.js
+++ jquery-apps/PasswordConfirmationForm/js/app.js
@@ -1,24 +1,37 @@
 //Problem: Hints are shown even when form is valid
 //Solution: Hide and show them at appropriate times
 
+var $password = $('#password')
+var $confirmPassword = $('#confirm_password')
+
 //Hide Hints
 $('form span').hide();
 
-function passordEvent() {
+function passwordEvent() {
     //Find out if password is valid
-    if($(this).val().length > 8) {
+    if($password.val().length > 8) {
         //Hide hint if valid
-        $(this).next().hide();
+        $password.next().hide();
     } else {
         //else show hint
-        $(this).next().show();
+        $password.next().show();
     }   
 }
 
+function confirmPasswordEvent() {
+    //Find out if password and confirmaiton match
+    if($password.val() === $confirmPassword.val()){
+        //Hide hint if match
+        $confirmPassword.next().hide();
+    } else {
+        //else show hint
+        $confirmPassword.next().show();
+    }     
+};
+
 //When event happens on password input
-$('#password').focus(passordEvent).keyup(passordEvent);
+$password.focus(passwordEvent).keyup(passwordEvent).focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
     
 //When event happens on confirmaiton input
-    //Find out if passord and confirmaiton match
-    //Hide hint if match
-    //else show hint
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
+    
