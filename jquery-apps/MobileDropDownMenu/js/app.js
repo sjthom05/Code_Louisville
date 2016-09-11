3bb2f3fc40962188c3460b48a3393ea8ed190c2f
Scott Thompson <sjthom05@gmail.com>
2016-09-10 20:12:33 -0400

    Createed a select menu and populated it.

--- jquery-apps/MobileDropDownMenu/js/app.js
+++ jquery-apps/MobileDropDownMenu/js/app.js
@@ -1,13 +1,23 @@
 //Problem: It look gross in smaller browser widths and small devices
 //Solution: To Hide the text links and swap them out with a more appropriate navigation
 
-//Create a select and append to menu
+//Create a select and append to #menu
+var $select = $("<select></select>");
+$('#menu').append($select);
 
 //Cycle over menu links
+$('#menu a').each(function(){
+    var $anchor = $(this);
     //Create an option
+    var $option = $('<option></option>');
     //Option's value is the href'
+    $option.val($anchor.attr('href'));
     //Option's text is the text of link'
+    $option.text($anchor.text());
     //Append option to select
+    $select.append($option);
+});
+    
 
 //Create button 
 
