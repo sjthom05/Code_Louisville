eacbd02f4f32278bb6deb469f21b3e4d5220ca54
Scott Thompson <sjthom05@gmail.com>
2016-09-10 21:00:13 -0400

    created password valadation event handler

--- jquery-apps/PasswordConfirmationForm/js/app.js
+++ jquery-apps/PasswordConfirmationForm/js/app.js
@@ -2,12 +2,22 @@
 //Solution: Hide and show them at appropriate times
 
 //Hide Hints
+$('form span').hide();
+
+function passordEvent() {
+    //Find out if password is valid
+    if($(this).val().length > 8) {
+        //Hide hint if valid
+        $(this).next().hide();
+    } else {
+        //else show hint
+        $(this).next().show();
+    }   
+}
 
 //When event happens on password input
-    //Find out if password is valid
-        //Hide hint if valid
-        //else show hint
-
+$('#password').focus(passordEvent).keyup(passordEvent);
+    
 //When event happens on confirmaiton input
     //Find out if passord and confirmaiton match
     //Hide hint if match
