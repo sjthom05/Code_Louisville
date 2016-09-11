4b2e40f6b9e5d7ce4d59c347f6756ad2c99fb999
Scott Thompson <sjthom05@gmail.com>
2016-09-10 22:39:10 -0400

    Plan out what the app will do

---
+++ jquery-apps/SimpleDrawingApplication/js/app.js
@@ -0,0 +1,19 @@
+//Problem: No user interaction causes no change to applicaiton
+//Solution: When user interacts cause changes appropriately
+
+//When clicking on control list items
+    //Deselect sibling elements
+    //Select clicked element
+
+//When new color is pressed
+    //Show #colorselect or hide #colorselect
+
+//When color sliders change
+    //update the new color span
+
+//When add color is pressed 
+    //append the color to the controls ul
+    //select the new color
+
+//On mouse events on the canvas
+    //Draw lines
