b37f89dd642310ef9ea27b3e5c04113eb264af7e
Scott Thompson <sjthom05@gmail.com>
2016-09-11 16:12:30 -0400

    Can now draw on the canvas.

--- jquery-apps/SimpleDrawingApplication/js/app.js
+++ jquery-apps/SimpleDrawingApplication/js/app.js
@@ -2,7 +2,10 @@
 //Solution: When user interacts cause changes appropriately
 
 var color = $(".selected").css('background-color');
-var context = $('canvas')[0].getContext('2d');
+var $canvas = $('canvas');
+var context = $canvas[0].getContext('2d');
+var lastEvent;
+var mouseDown = false;
 
 //When clicking on control list items
 $(".controls").on('click', 'li', function(){
@@ -45,11 +48,22 @@ $('#addNewColor').click(function(){
     
 
 //On mouse events on the canvas
-//Draw lines
-context.beginPath();
-context.moveTo(10,10);
-context.lineTo(20,10);
-context.lineTo(20,20);
-context.lineTo(10,20);
-context.closePath();
-context.stroke();
+
+$canvas.mousedown(function(e){
+    lastEvent = e;
+    mouseDown = true;
+}).mousemove(function(e){
+    //Draw lines
+    if(mouseDown){
+        context.beginPath();
+        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+        context.lineTo(e.offsetX, e.offsetY);
+        context.strokeStyle = color;
+        context.stroke();
+        lastEvent = e;
+    }
+}).mouseup(function(){
+    mouseDown = false;
+});
+
+
