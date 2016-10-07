fd0b2466dd97695883f9d2da067bc98178605feb
Scott Thompson <sjthom05@gmail.com>
2016-10-07 19:26:32 -0400

    selected elements

--- InteractiveWebPagesWithJavaScript/js/app.js
+++ InteractiveWebPagesWithJavaScript/js/app.js
@@ -1,7 +1,13 @@
 //Problem: User interaction dosen't provide desired results.
 //Solution: Add interactivity so the user can manage dail tasks.
 
+var taskInput = document.getElementById('new-task'); //new-task
+var addbutton = document.getElementsByTagName('button')[0]; //first button
+var incompleteTasksHolder = document.getElementById('incomplete-tasks'); //incomplete-tasks
+var completedTasksHolder = document.getElementById('completed-tasks'); //completed-tasks
+
 //Add a new task
+var addTask = function () {
     //When the button is pressed
     //Create a task
     //Create a new list item with the text from #new-task:
@@ -11,8 +17,11 @@
         //button.edit
         //button.delete
         //Each elements, needs modified and appened
+}
+
 
 //Edit an existing task
+var editTask = function () {
     //When the Edit button is pressed
         //if the class of the parent is .editMode
             //Switch from .editMode
@@ -21,18 +30,24 @@
             //Switch to .editMode
             //input value becomes the label's text
         //Toggle .editMode on the parent
+}
 
 
 //Delete an existing task
+var deleteTask = function () {
     //When the Delete button is pressed
         //Remove the parent list itemfrom the ul
+}
 
 
 //Mark a task as complete
+var taskCompleted = function () {
     ///When the checkbox is checked
         //Append the task list item to the completed-tasks
-
+}
 
 //Mark a task as incomplete
+var taskIncomplete = function () {
     //When the checkbox is unchecked
         //Apppend the task list item to the #incomplete-tasks
+}
