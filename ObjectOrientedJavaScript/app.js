dce3cca892539cc4632d45fb6d5f564d1fefdcdf
Scott Thompson <sjthom05@gmail.com>
2016-10-10 19:31:15 -0400

    finished quiz game

---
+++ ObjectOrientedJavaScript/app.js
@@ -0,0 +1,8 @@
+var questions = [
+    new Question("What is your quest?", ["To seek the Holy Grail", "I don't know"], "To seek the Holy Grail"),
+    new Question("What is air-speed velocity of an unladen swallow?", ["African?","European?"],"African")
+]
+
+var quiz = new Quiz(questions)
+
+QuizUI.displayNext();
