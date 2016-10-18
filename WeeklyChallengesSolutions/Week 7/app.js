13004562f60b700ea1082a2e3fe011cf49843291
Scott Thompson <sjthom05@gmail.com>
2016-10-18 18:59:16 -0400

    week 7 challeng

--- WeeklyChallengesSolutions/Week 7/app.js
+++ WeeklyChallengesSolutions/Week 7/app.js
@@ -1,4 +1,11 @@
 //See readme for challenge instructions
 var github = require('./github.js')
 
-github.getRepos('sjthom05')
+//var usernames = ["sjthom05"];
+var usernames = process.argv.slice(2);
+usernames.forEach(function(user){
+    console.log('User: '+user)
+    console.log('Repo List:')
+    github.getRepos(user)
+})
+//github.getRepos(username).forEach(function())
