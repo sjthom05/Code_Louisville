e5b927918f26b864e9ca99d444e7cedf4d85c8bd
Scott Thompson <sjthom05@gmail.com>
2016-10-23 16:35:10 -0400

    Treehouse Express Basics More base file changes

--- Express/src/app.js
+++ Express/src/app.js
@@ -1,29 +1,33 @@
 'use strict';
 
-var express = require('express');
-var posts = require('./mock/posts.json');
+var express = require('express'),
+	  posts = require('./mock/posts.json');
+
+var postsLists = Object.keys(posts).map(function(value) {
+							         return posts[value]})
+
 var app = express();
 
-app.use('/static', express.static( __dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public'))
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
 
 app.get('/', function(req, res){
-    res.render('index');
+	res.render('index');
 });
 
-app.get('/blog/:title?', function(req, res){
-    var title = req.params.title;
-    if(title === undefined){
-        res.status(503);
-        res.send("This page is under construction");
-    }else{
-        var post = posts[title] || {};
-        res.render('post', {post: post});
-    }
+app.get('/blog/:title?', function(req, res){ 
+	var title = req.params.title;
+	if (title === undefined) {
+		res.status(503);
+		res.render('blog', {posts: postsLists})
+	} else {
+		var post = posts[title] || {};
+		res.render('post', { post: post});
+	}
 });
 
-app.listen(3000 , function(){
-    console.log("The frontend server is running on port 3000!")
+app.listen(3000, function() {
+	console.log("The frontend server is running on port 3000!");
 });
