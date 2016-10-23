8fc6b9105b8e138eb040fd0b29ec0be9a1591ca7
Scott Thompson <sjthom05@gmail.com>
2016-10-23 13:59:20 -0400

    Treehouse Express Basics setting up routes and testing tools

--- Express/src/app.js
+++ Express/src/app.js
@@ -1,3 +1,17 @@
 'use strict';
 
 var express = require('express');
+var posts = require('./mock/posts.json');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('<h1>this is a test three</h1>')
+});
+
+app.get('/blog', function(req, res){
+    res.send(posts);
+});
+
+app.listen(3000 , function(){
+    console.log("The frontend server is running on port 3000!")
+});
