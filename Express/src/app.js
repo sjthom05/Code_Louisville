f00b21550c9089370d14935813c9c80d092b005a
Scott Thompson <sjthom05@gmail.com>
2016-10-23 14:38:41 -0400

    Treehouse Express Basics Updated the app.js to better handle the blog routes

--- Express/src/app.js
+++ Express/src/app.js
@@ -8,8 +8,15 @@ app.get('/', function(req, res){
     res.send('<h1>this is a test three</h1>')
 });
 
-app.get('/blog', function(req, res){
-    res.send(posts);
+app.get('/blog/:title?', function(req, res){
+    var title = req.params.title;
+    if(title === undefined){
+        res.status(503);
+        res.send("This page is under construction");
+    }else{
+        var post = posts[title];
+        res.send(post);
+    }
 });
 
 app.listen(3000 , function(){
